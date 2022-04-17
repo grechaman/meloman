63cdb504fdb078fc4ce158363eb127ec91e4adca
IvanGlushkov <VanesaNET@yandex.ru>
2022-04-17 14:50:35 +0700

    Чистка кода

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,30 +1,33 @@
 package ru.netology
 
 fun main() {
-    val conditionDiscount = 1000
-    val conditionDiscountSecondStep = 10000
-    val firstDiscount = 100
+    val conditionDiscount = 1000.0
+    val conditionDiscountSecondStep = 10000.0
+    val firstDiscount = 100.0
     val secondDiscount = 0.05
     val loyaltyDiscount = 0.01
 
-    class Person(val name: String, val age: Int, val amount: Int, val regular: Boolean)
+    class Person(val name: String, val age: Int, val amount: Double, val regular: Boolean)
 
-    val personAlex = Person("Alex", 18, 5000, true)
-    val personAdam = Person("Adam", 17, 50, false)
-    val personJohn = Person("John", 25, 15000, true)
-    val personKate = Person("Kate", 19, 1500, false)
+    val personAlex = Person("Alex", 18, 5000.0, true)
+    val personAdam = Person("Adam", 17, 50.0, false)
+    val personJohn = Person("John", 25, 15000.0, true)
+    val personKate = Person("Kate", 19, 1500.0, false)
 
-    fun discountCalculation(people: Person, amountBuy: Int):Int {
-        val amountPay = if (people.amount > conditionDiscount) {
+    fun discountCalculation(people: Person, amountBuy: Double): Double {
+        val amountPay: Double = if (people.amount > conditionDiscount) {
             if (people.amount > conditionDiscountSecondStep) {
                 amountBuy - amountBuy * secondDiscount
             } else amountBuy - firstDiscount
         } else amountBuy
-        return if (people.regular) (amountPay-(amountPay*loyaltyDiscount)) else amountPay
+        return when (people.regular) {
+            true -> amountPay * (1 - loyaltyDiscount)
+            false -> amountPay
+        }
     }
 
-    println(discountCalculation(personAlex, 2000))
-    println(discountCalculation(personAdam, 2000))
-    println(discountCalculation(personJohn, 5000))
-    println(discountCalculation(personKate, 2000))
+    println(discountCalculation(personAlex, 2000.0))
+    println(discountCalculation(personAdam, 2000.0))
+    println(discountCalculation(personJohn, 5000.0))
+    println(discountCalculation(personKate, 2000.0))
 }
